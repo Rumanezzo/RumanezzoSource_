a1ebe9b06343861f563a922d2b819c0dd172347d
Rumanezzo <rumanezzo@gmail.com>
2023-09-03 14:01:38 +0300

    ColorDemo1.py expand scaling and syntax

--- ColorDemo1.py
+++ ColorDemo1.py
@@ -1,19 +1,26 @@
 import turtle as t
 import colorsys as cs
 
-t.setup(800, 800)
+t.setup(0.99, 0.9, 0, 0)
+t.title('Секторами рисуем...')
+t.ht()
+
 t.speed('fastest')
 t.width(2)
 t.bgcolor('black')
 t.seth(45)
-n = 180
+
+n = 360
+n0 = 9
+
 for i in range(n):
     t.begin_fill()
-    t.color(cs.hsv_to_rgb(i/6, i/n, 0.8))
-    t.fillcolor(cs.hsv_to_rgb(i/6, i/n, 1))
+    t.color(cs.hsv_to_rgb(i/n0, i/n, 0.8))
+    t.fillcolor(cs.hsv_to_rgb(i/n0, i/n, 1))
     t.rt(90)
     t.circle(i*1.2, 90)
     t.end_fill()
-    t.rt(59)
-t.ht()
+    t.rt(360 // n0 - 1)
+    t.title(f'Секторами рисуем - {i}-й цикл!')
+
 t.done()
