795e2626bec9511450b69d54ffe50db3e85d4d70
Rumanezzo <rumanezzo@gmail.com>
2023-09-05 21:30:33 +0300

    TurtleSetup.py minor updates

--- TurtleSetup.py
+++ TurtleSetup.py
@@ -26,10 +26,10 @@ s0.mode('logo')  # Черепашка на север - мордой вверх!
 
 # Граничные точки
 step = 20
-xr = s0.window_width() // 2 - step  # Самая правая
-xl = -xr  # Самая левая
-yu = s0.window_height() // 2 - step  # Самая верхняя
-yd = -yu  # Самая нижняя
+x_right = s0.window_width() // 2 - step  # Самая правая
+x_left = -x_right  # Самая левая
+y_up = s0.window_height() // 2 - step  # Самая верхняя
+y_down = -y_up  # Самая нижняя
 
 s0.bgcolor('grey')
 # -------------------------------Конец инициализации экрана-------------------------------
@@ -44,10 +44,11 @@ n_colors = len(colors)
 
 if __name__ == '__main__':
 
-    def moving(x, y):
+    def moving(coord_x, coord_y, color):
         t0.ht()
         t0.pu()
-        t0.goto(x, y)
+        t0.goto(coord_x, coord_y)
+        t0.color(color)
         t0.shape('circle')
         t0.stamp()
         t0.st()
@@ -56,16 +57,25 @@ if __name__ == '__main__':
     t0.speed(6)
     t0.width(8)
     t0.pu()
-    t0.goto(xl, 0)
+    t0.goto(x_left, 0)
     t0.st()
     t0.pd()
 
+    title_output = '⋅'
     for i, c in enumerate(colors):
         t0.color(c)
-        t0.write(c, font=font)
-        s0.title(f'Проверяем работоспособность модуля...Всего цветов -> {n_colors - i} -> рисуем {c}')
+
+        title_output += f'{c}⋅'
+        s0.title(title_output)
         t0.circle(40)
-        t0.goto(xl + 50 * (i + 1), 0)
+        t0.bk(40)
+        if i % 2:
+            t0.lt(90)
+        else:
+            t0.rt(90)
+        t0.fd(40)
+        t0.write(c, font=font)
+        t0.goto(x_left + 50 * (i + 1), 0)
 
     t0.color('maroon')
     t0.home()
@@ -80,7 +90,7 @@ if __name__ == '__main__':
     t0.rt(180)
     t0.circle(200)
     t0.rt(90)
-    t0.color('burlywood')
+    t0.color('chartreuse')
     s0.title('Рисуем ушки...')
     t0.circle(30)
     t0.rt(180)
@@ -92,10 +102,10 @@ if __name__ == '__main__':
     t0.rt(180)
     t0.circle(120)
     s0.title('Расставляем точки над ё...')
-    moving(xl, yu)
-    moving(xr, yu)
-    moving(xl, yd)
-    moving(xr, yd)
+    moving(x_left, y_up, 'yellow')
+    moving(x_right, y_up, 'green')
+    moving(x_left, y_down, 'cyan')
+    moving(x_right, y_down, 'magenta')
 
     s0.title('Для завершения - кликнете на экран')
     s0.exitonclick()
